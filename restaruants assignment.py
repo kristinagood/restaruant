aa7b796b4c9f84864f9b98b760acd9381c83326a
kristinagood <79094550+kristinagood@users.noreply.github.com>
2021-10-08 11:43:06 -0400

    Add files via upload

--- restaruants assignment.py
+++ restaruants assignment.py
@@ -2,16 +2,23 @@ vegetarian = input('Is anyone at your party vegetarian? ').lower()
 vegan = input('Is anyone at your party vegan? ').lower()
 gluten_free = input('Is anyone at your party gluten free? ').lower()
 print('Here are your choices:')
-if vegetarian != 'yes' and vegan != 'yes' and gluten_free != 'yes':
+while vegetarian != 'yes' and vegan != 'yes' and gluten_free != 'yes':
     print('Joes Gourmet Burgers')
     print('Main Street Pizza Company')
     print('Mamas Fine Italian')
-if vegetarian != 'yes' and vegan != 'yes' and gluten_free == 'yes':
+while vegetarian != 'yes' and vegan != 'yes' and gluten_free == 'yes':
     print('Main Street Pizza Company')
-if vegetarian == 'yes' and vegan != 'yes' and gluten_free == 'yes':
+while vegetarian == 'yes' and vegan != 'yes' and gluten_free == 'yes':
     print('Main Street Pizza Company')
-if vegetarian == 'yes' and vegan != 'yes' and gluten_free != 'yes':
+while vegetarian == 'yes' and vegan != 'yes' and gluten_free != 'yes':
     print('Main Street Pizza Company')
     print('Mamas Fine Italian')
+
 print('Corners Cafe')
 print('The Chefs Kitchen')
+
+start = 'yes'
+stop = 'no'
+restaurant_search = input('Would you like to do another restaurant search? :')
+for x in range(vegetarian,vegan, gluten_free):
+    print(loop)
